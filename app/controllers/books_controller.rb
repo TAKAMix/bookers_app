2871233eaa8bef3cab614b3c48d2750274ea5444
EC2 Default User <ec2-user@ip-172-31-26-114.ap-northeast-1.compute.internal>
2022-07-12 12:09:40 +0000

    [Add] 3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,17 @@
 class BooksController < ApplicationController
   def new
+    @book=Book.new
   end
 
   def index
+  end
+  
+  def create
+    book = Book.index(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/top'
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   get 'books/new'
   get '/' => 'homes#top'
-  get 'books' => 'books#show'
-  get 'books' => 'books#edit'
   get 'books' => 'books#index'
+  post 'books' => 'books#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
